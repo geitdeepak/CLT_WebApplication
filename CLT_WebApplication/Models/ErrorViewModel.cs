06ca4661c78f8a7943a95b6b1beece379e98df23
USER <USER@DESKTOP-V0R3O1R>
2023-12-12 17:19:25 +0530

    Add project files.

---
+++ CLT_WebApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CLT_WebApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ CLT_WebApplication/wwwroot/js/site.js
@@ -0,0 +1,13 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+<script>
+    function toggleNavbar() {
+        var x = document.querySelector(".navbar");
+    if (x.className.indexOf("responsive") === -1) {
+        x.className += " responsive";
+        } else {
+        x.className = x.className.replace(" responsive", ""); }
+    }
+</script>
